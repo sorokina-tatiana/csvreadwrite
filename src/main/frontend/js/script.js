f2afcc340584810e154b0f3ed17f900223ee2789
tatiana <sorokina.tatiana.ev@gmail.com>
2019-06-03 19:58:04 +0200

    add buttons in table-rows: Save and Edit

--- src/main/frontend/js/script.js
+++ src/main/frontend/js/script.js
@@ -1,7 +1,7 @@
 (function() {
     var uploadButton = document.getElementById('upload-button');
-    var tableBody = document.getElementById('table-body');
-    var tableHead = document.getElementById("table-head")
+    var downloadButton = document.getElementById('download-button');
+
     uploadButton.addEventListener('click', function(event) {
         event.preventDefault();
         var data = new FormData();
@@ -12,26 +12,77 @@
                 'Content-Type': 'multipart/form-data'
             }
         }).then(function(res) {
-            console.log(res);
-            var tableHeader = document.createElement('tr');
-            Object.keys(res.data[0] || []).forEach(function(key) {
-                var th = document.createElement('th');
-                th.textContent = key;
-                tableHeader.appendChild(th);
-            });
-            tableHead.appendChild(tableHeader);
-
-            res.data.forEach(function(record) {
-                var tr = document.createElement('tr');
-                Object.keys(record || []).forEach(function(key) {
-                    var td = document.createElement('td');
-                    td.textContent = record[key];
-                    tr.appendChild(td);
-                });
-                tableBody.appendChild(tr);
-            });
+            generateTableHeader(res.data);
+            generateTableBody(res.data);
         }).catch(function(err) {
             console.log(err)
         });
     });
+
+    downloadButton.addEventListener('click', function() {
+        axios.get('/download');
+    });
+
+    var createButton = function(type, callback) {
+        var btn = document.createElement('button');
+        btn.textContent = type;
+        btn.addEventListener('click', callback);
+        return btn;
+    };
+
+    var editFields = function(event) {
+        console.log(event.target);
+    };
+
+    var saveChanges = function(event) {
+        var data;
+
+        axios.post('/save-entry', data)
+            .then(function(res) {
+                console.log(res);
+            })
+            .catch(function(err) {
+                console.log(err);
+            })
+    };
+
+    var generateTableHeader = function(data) {
+        var tableHead = document.getElementById('table-head');
+        var tableHeaderRow = document.createElement('tr');
+
+        Object.keys(data[0] || []).forEach(function(key) {
+            var th = document.createElement('th');
+            th.textContent = key;
+            tableHeaderRow.appendChild(th);
+        });
+
+        var th = document.createElement('th');
+        th.textContent = 'Actions';
+        tableHeaderRow.appendChild(th);
+        tableHead.appendChild(tableHeaderRow);
+    };
+
+    var generateTableBody = function(data) {
+        var tableBody = document.getElementById('table-body');
+
+        data.forEach(function(record) {
+            var tr = document.createElement('tr');
+
+            Object.keys(record || []).forEach(function(key) {
+                var td = document.createElement('td');
+                td.textContent = record[key];
+                tr.appendChild(td);
+            });
+            var td = document.createElement('td');
+
+            var btnEdit = createButton('Edit', editFields);
+            td.appendChild(btnEdit);
+
+            var btnSave = createButton('Save', saveChanges);
+            td.appendChild(btnSave);
+            tr.appendChild(td);
+
+            tableBody.appendChild(tr);
+        });
+    }
 })();
